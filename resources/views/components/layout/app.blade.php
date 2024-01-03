13d078449e7990fa1bab7892a044102d60abcbdd
“Frank <frank@frankjones.co>
2024-01-03 15:29:58 +0000

    Site Navigation is good.

---
+++ resources/views/components/layout/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="{{asset('images/favicon-94x94.png')}}">
+    
+
+    @meta_tags
+
+    {{-- <meta property="og:site_name" content="Colby Fayock" />
+    <meta property=“og:title” content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:url" content="{{url()->current()}}" />
+    <meta property="og:type" content="article" />
+    <meta property="article:publisher" content="https://truecrimemetrix.com" />
+    <meta property="article:section" content="Coding" />
+    <meta property="article:tag" content="Coding" />
+    <meta property="og:image" content="" />
+    <meta property="og:image:width" content="1280" />
+    <meta property="og:image:height" content="640" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:image" content="" />
+    <meta property="twitter:site" content="@truecrimemetrix" /> --}}
+
+
+    {{-- GOOGLE FONTS --}}
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,400;0,600;0,700;1,300;1,400;1,600&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');">
+
+
+    <!-- GOOGLE ADSENSE -->
+
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5443411235770747"
+     crossorigin="anonymous"></script>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body>
+    <x-layout.navigation />
+    <main>
+        {{$slot}}
+    </main>
+    <x-layout.footer />
+    <x-blocks.blackout />
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +12,35 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+|
+--------------------------------------------------------------------------
+| Key Definitions
+|--------------------------------------------------------------------------
+|
+|   INDEX       List records
+|   VIEW........View single page
+|   SHOW........Show single record
+|   CREATE......Show form for creating a record
+|   STORE.......Save record to the database
+|   EDIT........Show form to edit record
+|   UPDATE......Save updated record to database
+|   DESTROY.....Delete record from database
+|
+|--------------------------------------------------------------------------
+|
+**************************************************************************/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+// SITE CONTROLLER
+
+    // ALL USERS
+
+    Route::controller(SiteController::class)->group(function(){
+        
+        Route::get('/', 'viewHome');
+        
+    });
+
+
